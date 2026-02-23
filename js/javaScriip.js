20da97e40ae437f0d17b0ee41f8bbaa40a671b81
678-juan <juanfvg468@gmail.com>
2026-02-23 08:37:07 -0500

    AInformacionGenral creado

--- js/javaScriip.js
+++ js/javaScriip.js
@@ -4,3 +4,14 @@ const btnSiguiente = document.getElementById("btn-siguiente");
 checkAcepto.addEventListener("change", function () {
 	btnSiguiente.disabled = !checkAcepto.checked;
 });
+
+btnSiguiente.addEventListener("click", function () {
+	if (btnSiguiente.disabled) {
+		return;
+	}
+
+	const nextPage = btnSiguiente.getAttribute("data-href");
+	if (nextPage) {
+		window.location.href = nextPage;
+	}
+});
